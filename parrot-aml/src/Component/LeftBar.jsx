52b9c1071186d43b688491b9374b9382e81b7042
legendcode1 <68510188+legendcode1@users.noreply.github.com>
2025-03-13 15:18:25 +0700

    Reapply "Add Moderator and Editable User components with styles; refactor ChatBot and MainInterface components"
    This reverts commit efefbac1ced94fa88fd52c226b064d09405dbd6f.

--- parrot-aml/src/Component/LeftBar.jsx
+++ parrot-aml/src/Component/LeftBar.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import '../StyleSheet/LeftBar.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { loadChatMessagesFirestore } from '../indexedDB';
 import { fetchChatHistoryByCompanyID } from '../auth/auth';
 import PropTypes from 'prop-types';
 import ChatHistoryList from './ChatHistoryList';
+import ProfileSettings from './ProfileSettings';
 
 import plusIcon from '/leftbar/plus.svg';
 import datum from '/leftbar/datum.png';
@@ -13,6 +14,7 @@ const LeftBar = ({ clientId }) => {
   const [chatHistory, setChatHistory] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [activeSection, setActiveSection] = useState('chatHistory');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,7 +22,6 @@ const LeftBar = ({ clientId }) => {
       try {
         setLoading(true);
         const firestoreChats = await fetchChatHistoryByCompanyID(clientId);
-        // Optionally update IndexedDB with the latest Firestore data
         await loadChatMessagesFirestore(firestoreChats);
         setChatHistory(firestoreChats);
       } catch (err) {
@@ -41,7 +42,7 @@ const LeftBar = ({ clientId }) => {
 
   const handlePlusClick = () => {
     console.log('Plus icon clicked. Navigating to /dashboard');
-    navigate('/dashboard'); // Navigate back to the dashboard (e.g., NewReport view)
+    navigate('/dashboard');
   };
 
   if (loading) return <div className="loading">Loading chats...</div>;
@@ -49,7 +50,6 @@ const LeftBar = ({ clientId }) => {
 
   return (
     <div className="left-bar-container">
-      {/* Logo and Plus Icon */}
       <div className="logo-container">
         <img src={datum} alt="Datum Logo" className="logo" />
         <img
@@ -61,8 +61,15 @@ const LeftBar = ({ clientId }) => {
         />
       </div>
       <hr />
-      {/* Render the ChatHistoryList (chat history) */}
-      <ChatHistoryList chatHistory={chatHistory} />
+      {activeSection === 'chatHistory' ? (
+        <ChatHistoryList
+          chatHistory={chatHistory}
+          onShowProfileSettings={() => setActiveSection('profile')}
+          clientId={clientId}
+        />
+      ) : (
+        <ProfileSettings onGoBack={() => setActiveSection('chatHistory')} />
+      )}
     </div>
   );
 };
